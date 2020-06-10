254ba7b469cf43fe10582c57a6e04ad6e0fe1a49
Tyler Nijmeh <tylernij@gmail.com>
2020-06-10 15:13:46 -0700

    Disable heap randomization
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- AnyKernel3/CompanionModule/service.sh
+++ AnyKernel3/CompanionModule/service.sh
@@ -8,3 +8,6 @@ resetprop ctl.stop oneplus_brain_service
 sysctl -w vm.dirty_ratio=50
 sysctl -w vm.dirty_background_ratio=10
 sysctl -w vm.vfs_cache_pressure=50
+
+# Disable heap randomization
+sysctl -w kernel.randomize_va_space=0
