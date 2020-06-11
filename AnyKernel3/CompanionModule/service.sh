e5ea91d8e09087ecc7588473a8d2a2a439e2d9a0
Tyler Nijmeh <tylernij@gmail.com>
2020-06-11 10:53:05 -0700

    Increase vfs cache pressure a bit
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- AnyKernel3/CompanionModule/service.sh
+++ AnyKernel3/CompanionModule/service.sh
@@ -7,7 +7,7 @@ resetprop ctl.stop oneplus_brain_service
 # Memory tweaks
 sysctl -w vm.dirty_ratio=50
 sysctl -w vm.dirty_background_ratio=10
-sysctl -w vm.vfs_cache_pressure=50
+sysctl -w vm.vfs_cache_pressure=80
 
 # Disable heap randomization
 sysctl -w kernel.randomize_va_space=0
