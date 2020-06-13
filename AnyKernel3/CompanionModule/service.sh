8860516819050d69de3e896f1457fcd7cf63ab11
Tyler Nijmeh <tylernij@gmail.com>
2020-06-12 17:53:54 -0700

    Process child forks before the parent
    A study from RedHat shows that enabling sched_child_runs_first reduces,
    on average, the usecs per call on forks and exits.
    
    https://www.redhat.com/files/summit/session-assets/2018/Performance-analysis-and-tuning-of-Red-Hat-Enterprise-Linux-Part-1.pdf
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- AnyKernel3/CompanionModule/service.sh
+++ AnyKernel3/CompanionModule/service.sh
@@ -11,3 +11,6 @@ sysctl -w vm.vfs_cache_pressure=80
 
 # Disable heap randomization
 sysctl -w kernel.randomize_va_space=0
+
+# Process child forks before the parent
+sysctl -w kernel.sched_child_runs_first=1
