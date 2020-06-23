539ad725cc606316b66620694161d37280e778b6
Tyler Nijmeh <tylernij@gmail.com>
2020-06-22 23:13:42 -0700

    Use lower min freq for policy0
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- AnyKernel3/CompanionModule/service.sh
+++ AnyKernel3/CompanionModule/service.sh
@@ -6,3 +6,10 @@ sysctl -w kernel.randomize_va_space=0
 
 # Process child forks before the parent
 sysctl -w kernel.sched_child_runs_first=1
+
+# --- DELAYED CHANGES ---
+sleep 30
+
+# Use lowest available frequency on policy0
+echo 300000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
+
