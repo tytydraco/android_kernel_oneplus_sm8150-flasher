7abc315f6dfdf6e910a7605e1895c191cce67f1d
Tyler Nijmeh <tylernij@gmail.com>
2020-07-30 07:05:31 +0000

    Update for new kernel rebase
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- AnyKernel3/companion_module/service.sh
+++ AnyKernel3/companion_module/service.sh
@@ -1,25 +1,8 @@
 #!/bin/sh
 # Draco Kernel Companion Module
 
-# Disable dmesg
-echo off > /proc/sys/kernel/printk_devkmsg
-
-# Disable heap randomization
-sysctl -w kernel.randomize_va_space=0
-
 # Process child forks before the parent
 sysctl -w kernel.sched_child_runs_first=1
 
-# Use kernel default dirty page max expiration time
-sysctl -w vm.dirty_expire_centisecs=3000
-
 # Adjust schedtune configuration
 echo 1 > /dev/stune/top-app/schedtune.crucial
-echo 1 > /dev/stune/top-app/schedtune.prefer_idle
-
-# --- DELAYED CHANGES ---
-sleep 30
-
-# Use lowest available frequency on policy0
-echo 300000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
-
